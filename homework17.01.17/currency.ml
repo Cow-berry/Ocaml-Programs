367f6d951f0a515baf454ee0b04b2c0f38f2f5d6
cowberry <tolya.cow@gmail.com>
2017-01-24 10:49:22 +0300

    Homework is done

--- homework17.01.17/currency.ml
+++ homework17.01.17/currency.ml
@@ -1,43 +1,27 @@
 type currency =
-  RUB of float|
-  USD of float;;
+  Rub of float|
+  Usd of float;;
 let course = 59.5605;;
 let rub = 10.;;
 let usd = 15.;;
 
-let print_rub c = Printf.printf "rub : %f \n" c;;
-let print_usd c = Printf.printf "usd : %f \n" c;;
 
-let in_rub c =
-  if (c <= usd && c >= 0.) then (
-    Printf.printf "Вы перевели в рубли %f долларов.\n Сейчас на балансе : \n %f рублей\n %f долларов\n" c (rub +. c *. course) (usd -. c)
-    ) else if c < 0. then (
-      print_string"Вы ввели отрицательное значение перевода.\n"
-    ) else (
-      print_string "У вас на балансе нет столько долларов.\n"
-    );;
 
 let in_usd c =
-  if (c <= rub && c >= 0.) then (
-    Printf.printf "Вы перевели в доллары %f рублей.\n Сейчас на балансе : \n %f рублей\n %f долларов\n" c (rub -. c) (usd +. c /. course)
-    ) else if c < 0. then (
-      print_string"Вы ввели отрицательное значение перевода.\n"
-    ) else
-    (print_string "У вас на балансе нет столько рублей.\n");;
+  match c with
+    Rub (x) -> x /. course
+    |Usd (x) -> x ;;
 
-print_rub rub;;
-print_usd usd;;
-print_string "Сколько вы хотите перевести из долларов в рубли : "
-let c = read_float();;
-in_rub c;;
-if (c <= usd && c >= 0) then (
-  rub = rub +. c *. course;;
-  usd = usd -. c;;
-  );;
-print_string "Сколько вы хотите перевести из рублей в доллары : "
-let c = read_float();;
-in_usd c;;
-if (c <= rub && c >= 0.) then (
-  let rub = rub -. c;;
-  let usd = usd +. c /. course;;
-  );;
+let in_rub c =
+  match c with
+    Rub (x) -> x
+    |Usd (x) -> x *. course;;
+
+print_string "Введите 1 если хотите перевести рубли в доллары. 2 если хотите перевести доллары в рубли."
+let q = read_int();;
+if q = 1 then
+  let x = read_float() in
+  print_float(in_usd (Rub x))
+else if q = 2 then
+  let x = read_float() in
+  print_float(in_rub (Usd x));;
